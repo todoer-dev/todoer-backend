830bbd5fe494453c072c5bc289487f28da7148f0
in-hale <mkrvch.yury@gmail.com>
2019-11-24 18:06:46 +0300

    Configure CORS

--- config/application.rb
+++ config/application.rb
@@ -21,6 +21,12 @@ Bundler.require(*Rails.groups)
 
 module Todoer
   class Application < Rails::Application
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins ENV.fetch('FRONTEND_APP_ORIGIN', '*')
+        resource '*', headers: :any, methods: [:get, :post, :options]
+      end
+    end
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
