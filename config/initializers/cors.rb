eab3ef2c56f27bad6bd9de5e93302e942ed86c51
in-hale <mkrvch.yury@gmail.com>
2020-01-05 23:54:10 +0300

    Allow all http methods by CORS

--- config/initializers/cors.rb
+++ config/initializers/cors.rb
@@ -1,16 +1,8 @@
-# Be sure to restart your server when you modify this file.
+# frozen_string_literal: true
 
-# Avoid CORS issues when API is called from the frontend app.
-# Handle Cross-Origin Resource Sharing (CORS) in order to accept cross-origin AJAX requests.
-
-# Read more: https://github.com/cyu/rack-cors
-
-# Rails.application.config.middleware.insert_before 0, Rack::Cors do
-#   allow do
-#     origins 'example.com'
-#
-#     resource '*',
-#       headers: :any,
-#       methods: [:get, :post, :put, :patch, :delete, :options, :head]
-#   end
-# end
+Rails.application.config.middleware.insert_before 0, Rack::Cors do
+  allow do
+    origins ENV.fetch('FRONTEND_APP_ORIGIN', '*')
+    resource '*', headers: :any, methods: :any
+  end
+end
